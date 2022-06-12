e68c3b35d7fa1ba1c7be120ad7939258a97ef6e2
dimasrdwiaji <79129785+dimasrdwiaji@users.noreply.github.com>
2022-06-12 16:58:40 +0700

    buat promiseOutput dan fungsi hitung jumlah emosi

--- promise.js
+++ promise.js
@@ -1,7 +1,55 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  // Buat promise baru
+  return new Promise ((resolve, reject) => {
+    // Promise akan reject kalau tidak ada data/ data null/ data undefined
+    if (emosi === null || emosi === '') {
+      reject ('Tidak ada keterangan emosi');
+      
+      // Hasil resolve bila emosi tidak marah
+    } else if (emosi == 'tidak marah') {
+      (async () => {
+        // Hitung jumlah emosi tidak marah di tiap theater lalu hasilnya dijumlah
+        let hitungIIX = hitung(await promiseTheaterIXX(), emosi);
+        let hitungVGC = hitung(await promiseTheaterVGC(), emosi);
+        resolve(hitungIIX + hitungVGC);
+      })();
+
+      // Hasil resolve bila emosi marah
+    } else if (emosi == 'marah') {
+      (async () => {
+        // Hitung jumlah emosi marah di tiap theater lalu hasilnya dijumlah
+        let hitungIIX = hitung(await promiseTheaterIXX(), emosi);
+        let hitungVGC = hitung(await promiseTheaterVGC(), emosi);
+        resolve(hitungIIX + hitungVGC)
+      })();
+    }
+  })
+}
+
+// Fungsi hitung jumlah emosi marah dan jumlah emosi tidak marah di tiap theater
+const hitung = (theater, emosi) => {
+  // Buat variabel kosong
+  let jumlah = null;
+
+  if (emosi == 'tidak marah') {
+    for (const element of theater) {
+      if (element.hasil == 'tidak marah') {
+        jumlah++;
+      }
+    }
+  } else if (emosi == 'marah') {
+    for (const element of theater) {
+      if (element.hasil == 'marah') {
+        jumlah++
+      }
+    }
+  }
+  return jumlah;
+}
+
 
 module.exports = {
   promiseOutput,
